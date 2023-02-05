7685779748065bcd936824cd65874dafbaefa277
Tiago <sathoskath@gmail.com>
2023-02-04 22:03:48 -0300

    updated folder

--- Aula-01/01-fundamentos-reactjs/src/App.jsx
+++ Aula-01/01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,32 @@
-import { Post } from './Post'
-import { Header } from './components/Header'
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
 import './global.css';
+import styles from './App.module.css';
+
 
 function App() {
     return (
         <div>
             <Header />
-            <Post author="Joao" content="testetestesteste" />
+            
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    <Post 
+                        author="Tiago" 
+                        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ratione ea earum aliquam quos aliquid! Qui, nesciunt perspiciatis eligendi earum iusto unde assumenda. Officia voluptatibus voluptates incidunt quas." 
+                    />
+                    <Post 
+                        author="Tiago" 
+                        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ratione ea earum aliquam quos aliquid! Qui, nesciunt perspiciatis eligendi earum iusto unde assumenda. Officia voluptatibus voluptates incidunt quas." 
+                    />
+                </main>
+            </div>
         </div>
     )
 }
+
 
 export default App 
