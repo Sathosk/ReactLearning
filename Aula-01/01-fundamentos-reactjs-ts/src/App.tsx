0c84fd28d42ddb227104d5ef43a1b41ef4ddcada
Tiago <sathoskath@gmail.com>
2023-02-09 07:16:44 -0300

    update folder

---
+++ Aula-01/01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,66 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import './global.css';
+import styles from './App.module.css';
+
+// Mock data
+
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: 'https://github.com/sathosk.png',
+            name: 'Tiago Cruz',
+            role: 'Fullstack Web Developer'
+        },
+        content: [
+            { type: "paragraph", content: 'Fala galeraa 👋' } as const,
+            { type: "paragraph", content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' } as const,
+            { type: "link", content: '👉 jane.design/doctorcare' } as const
+        ],
+        publishedAt: new Date('2023-02-01 20:08:00'),
+    },
+    {
+        id: 2,
+        author: {
+            avatarUrl: 'https://github.com/AleMFS.png',
+            name: 'Alexandre',
+            role: 'Frontend Web Developer'
+        },
+        content: [
+            { type: "paragraph", content: 'Fala galeraa 👋' } as const,
+            { type: "paragraph", content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' } as const,
+            { type: "link", content: '👉 jane.design/doctorcare' } as const
+        ],
+        publishedAt: new Date('2023-02-06 16:34:00'),
+    },
+]
+
+function App() {
+    return (
+        <div>
+            <Header />
+
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    {posts.map(post => {
+                        return (
+                            <Post
+                                key={post.id}
+                                author={post.author}
+                                content={post.content}
+                                publishedAt={post.publishedAt}
+                            />
+                        )
+                    })}
+                </main>
+            </div>
+        </div>
+    )
+}
+
+
+export default App 
---
+++ Aula-01/01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+    src: string;
+}
+
+export function Comment(props: CommentProps) {
+    ////// State Variables
+    const [likeCount, setLikeCount] = useState(0);
+
+    ////// Handler Functions 
+    function handleDeleteComment() {
+        props.onDeleteComment(props.content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={ styles.comment }>
+            <Avatar hasBorder={false} src={ props.src }/>
+
+            <div className={ styles.commentBox }>
+                <div className={ styles.commentContent }>
+                    <header>
+                        <div className={ styles.authorAndTime }>
+                            <strong>Alexandre</strong>
+                            <time title='11 de maio ás 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={ handleDeleteComment } title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{props.content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={ handleLikeComment }>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>{ likeCount }</span> 
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
